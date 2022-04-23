195e63586a0ed44a964374c9a68ca88167adeb77
Matteo Penna <penna.matteo@gmail.com>
2022-04-23 21:31:26 +0200

    Added ingredients to recipe model

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,15 +1,36 @@
 import { EventEmitter, Injectable } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 import { Recipe } from "./recipe.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
 
+@Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('Test Recipe', 'This is simply a test', 
-    'http://foodjaunts.com/wp-content/uploads/2012/04/Spring-Shrimp-Salad-Overview-e1335228914177.jpg')
+    new Recipe('Tasty Schnitzel', 
+      'A super-tasty schnitzel - just awesome!', 
+      'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]),
+    new Recipe('Big Fat Burger', 
+      'What else you need to say?', 
+      'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1)
+      ])
   ];
 
-  getRecipe() {
+  constructor(private shoppingListService: ShoppingListService) {}
+
+  getRecipes() {
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    
   }
 }
