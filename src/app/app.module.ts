d4b98fe9be9296b06c2feb2a8e1bf6ccf88d27cf
Matteo Penna <penna.matteo@gmail.com>
2022-06-21 21:51:22 +0200

    Added authentication - step 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,10 +17,12 @@ import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.compon
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { RecipeService } from './recipes/recipe.service';
+import { AuthComponent } from './auth/auth.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    AuthComponent,
     HeaderComponent,
     RecipesComponent,
     RecipeListComponent,
